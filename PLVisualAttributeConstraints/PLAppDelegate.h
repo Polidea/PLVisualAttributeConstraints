3b6657c9aad9f396aeba23408b00eec7de9d3d79
Kamil Jaworski <kamil.jaworski@gmail.com>
2013-07-18 14:32:12 +0200

    Initial Commit

---
+++ PLVisualAttributeConstraints/PLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLAppDelegate.h
+//  PLVisualAttributeConstraints
+//
+//  Created by Kamil Jaworski on 18.07.2013.
+//  Copyright (c) 2013 Polidea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PLVisualAttributeConstraints/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PLVisualAttributeConstraints
+//
+//  Created by Kamil Jaworski on 18.07.2013.
+//  Copyright (c) 2013 Polidea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PLAppDelegate class]));
+    }
+}
